058fb431741e14f9e66c99943740d831e20a5ec4
ramyadav10 <ramyadav.now@gmail.com>
2021-08-21 18:29:03 +0530

    Calculating employee daily wage

--- main.sh
+++ main.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
+wagePerHr=20
+fullDay=8
+
 ranFun=$((RANDOM%2))
 
 if [[ $ranFun -eq 1 ]]
 then
 	echo "Present"
+	dailyWage=$(( $wagePerHr * $fullDay ))
+	echo $dailyWage
 else
 	echo "Absent"
 fi
